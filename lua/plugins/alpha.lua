2e48b32b43245add142e98092e6a2812d342fca1
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-11-26 20:30:03 +0300

    Fix alpha dashboard icons not displayed correctly

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -44,15 +44,15 @@ return {
 
       -- Menu
       dashboard.section.buttons.val = {
-        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("p", "  Recent projects", ":Telescope projects <CR>"),
-        dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+        dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
+        dashboard.button("e", "󰈔  New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("p", "󰏓  Recent projects", ":Telescope projects <CR>"),
+        dashboard.button("r", "󰄉  Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("c", "󰒓  Configuration", ":e $MYVIMRC <CR>"),
         dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
-        dashboard.button("m", "  Mason", ":Mason<CR>"),
-        dashboard.button("q", "  Quit", ":qa<CR>"),
+        dashboard.button("m", "󰏖  Mason", ":Mason<CR>"),
+        dashboard.button("q", "󰈆  Quit", ":qa<CR>"),
       }
 
       -- Footer
