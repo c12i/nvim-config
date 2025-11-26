01dee90c09babce10698ac41be49c93f2f0b8d53
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-11-26 18:56:49 +0300

    Remove eslint formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -47,33 +47,9 @@ return {
     end,
   },
 
-  -- Linting
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-
-      lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        python = { "pylint" },
-      }
-
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
-
   -- SchemaStore for JSON/YAML schemas
   {
     "b0o/schemastore.nvim",
     lazy = true,
   },
 }
-
