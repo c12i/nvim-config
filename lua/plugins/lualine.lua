90b1478c8030e1876e94433115dd4dbdddf8f135
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-12-11 16:53:54 +0300

    feat: include lsp client names in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,20 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
     config = function()
+      local function get_lsp_clients()
+        local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+        if next(buf_clients) == nil then
+          return ""
+        end
+
+        local client_names = {}
+        for _, client in pairs(buf_clients) do
+          table.insert(client_names, client.name)
+        end
+
+        return "  " .. table.concat(client_names, ", ")
+      end
+
       require("lualine").setup({
         options = {
           theme = "gruvbox",
@@ -18,7 +32,12 @@ return {
           },
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = { { "filename", path = 1 } },
-          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_x = {
+            { get_lsp_clients, color = { gui = "italic" } },
+            "encoding",
+            "fileformat",
+            "filetype"
+          },
           lualine_y = { "progress" },
           lualine_z = { "location" },
         },
@@ -26,4 +45,3 @@ return {
     end,
   },
 }
-
