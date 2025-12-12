f54935436338e175d1846ea9b0c28ac2f4658581
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-12-12 13:53:51 +0300

    feat: Add telescope based split and vsplit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,29 +9,51 @@ return {
     },
     keys = {
       -- Find
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
-      { "<leader>fn", "<cmd>enew<cr>", desc = "New File" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>fp", "<cmd>Telescope projects<cr>", desc = "Projects" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",                desc = "Find File" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                  desc = "Recent Files" },
+      { "<leader>fn", "<cmd>enew<cr>",                                desc = "New File" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",                   desc = "Buffers" },
+      { "<leader>fp", "<cmd>Telescope projects<cr>",                  desc = "Projects" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>",                 desc = "Help Tags" },
 
       -- Search
-      { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Text" },
+      { "<leader>st", "<cmd>Telescope live_grep<cr>",                 desc = "Text" },
       { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope commands<cr>", desc = "Commands" },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
-      { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>sc", "<cmd>Telescope commands<cr>",                  desc = "Commands" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                 desc = "Help" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                   desc = "Keymaps" },
+      { "<leader>sr", "<cmd>Telescope resume<cr>",                    desc = "Resume" },
 
       -- Git
-      { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
-      { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git branches" },
-      { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<cr>",               desc = "Git commits" },
+      { "<leader>gb", "<cmd>Telescope git_branches<cr>",              desc = "Git branches" },
+      { "<leader>gs", "<cmd>Telescope git_status<cr>",                desc = "Git status" },
     },
     config = function()
       local telescope = require("telescope")
       local actions = require("telescope.actions")
+      local builtin = require('telescope.builtin')
+
+      -- Split-specific file finding
+      vim.keymap.set('n', '<leader>fv', function()
+        builtin.find_files({
+          attach_mappings = function(_, map)
+            map('i', '<CR>', require('telescope.actions').file_vsplit)
+            map('n', '<CR>', require('telescope.actions').file_vsplit)
+            return true
+          end
+        })
+      end, { desc = 'Find files → vsplit' })
+
+      vim.keymap.set('n', '<leader>fs', function()
+        builtin.find_files({
+          attach_mappings = function(_, map)
+            map('i', '<CR>', require('telescope.actions').file_split)
+            map('n', '<CR>', require('telescope.actions').file_split)
+            return true
+          end
+        })
+      end, { desc = 'Find files → split' })
 
       telescope.setup({
         defaults = {
@@ -62,4 +84,3 @@ return {
     end,
   },
 }
-
