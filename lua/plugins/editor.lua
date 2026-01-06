1a7aba15a6f07720bcf984607799975126119575
Collins Muriuki <hello@collinsmuriuki.xyz>
2026-01-06 15:12:00 +0300

    feat: Add auto-darkmode to sync theme to system

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -166,6 +166,22 @@ return {
     end,
   },
 
+  -- Auto Dark Mode
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = function()
+      require("auto-dark-mode").setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.opt.background = "dark"
+        end,
+        set_light_mode = function()
+          vim.opt.background = "light"
+        end,
+      })
+    end,
+  },
+
   -- Terminal
   {
     "akinsho/toggleterm.nvim",
