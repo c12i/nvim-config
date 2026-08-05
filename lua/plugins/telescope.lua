c0b0992708460841856ec078e238ff0efdb2dd28
Collins Muriuki <hello@collinsmuriuki.xyz>
2026-08-05 13:11:31 +0300

    Fix telescope width mismatch between selection_caret and entry_prefix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -68,7 +68,7 @@ return {
       telescope.setup({
         defaults = {
           prompt_prefix = " ",
-          selection_caret = " ",
+          selection_caret = "  ",
           path_display = { "truncate" },
           file_ignore_patterns = { ".git/", "node_modules", "target", "dist", "build" },
           mappings = {
